17b78d543a6c9da5fa05f87a79ae900b188d3d82
MinjiJun <seonho@MinjiJunui-MacBookPro.local>
2024-02-17 20:38:38 +0900

    Create project

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+    <Logo></Logo>
+    <a href="https://kimseonho.co.kr" target="_blank">
+      (c){{ new Date().getFullYear() }} HEROPY
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from "~/components/Logo";
+
+export default {
+  components: {
+    Logo,
+  },
+};
+</script>
+
+<style>
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: 0.3;
+  .logo {
+    display: block;
+    margin-bottom: 4px;
+  }
+}
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+    namespaced: true,
+    state: () => ({
+        name : 'HEROPY',
+        email: "minji.jun.85@gmail.com",
+        blog: "https://heropy.blog",
+        phone: "010-3049-7855",
+        image: "http://heropy.blog/css/images/logo.png"
+    })
+}
